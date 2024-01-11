13aa1bdb919649f97cc3a120a8bbae05de3f90a2
ping0807 <93776380+ping0807@users.noreply.github.com>
2024-01-11 17:13:57 +0800

    calories2

--- lib/view/calories/caloriestracker.dart
+++ lib/view/calories/caloriestracker.dart
@@ -1,8 +1,8 @@
-// calories_tracker_page.dart
-
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
+import 'piechart.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'addcalories.dart';  // Import the AddCaloriesPage
 
 class CaloriesTrackerPage extends StatefulWidget {
   @override
@@ -11,6 +11,17 @@ class CaloriesTrackerPage extends StatefulWidget {
 
 class _CaloriesTrackerPageState extends State<CaloriesTrackerPage> {
   // TODO: Define necessary variables and controllers
+  late DateTime _selectedDate;
+  late DateTime _firstDay;
+  late DateTime _lastDay;
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedDate = DateTime.now();
+    _firstDay = DateTime(2022, 1, 1);  // Replace with your desired start date
+    _lastDay = DateTime(2022, 12, 31);  // Replace with your desired end date
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,18 +35,38 @@ class _CaloriesTrackerPageState extends State<CaloriesTrackerPage> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             // TODO: Add Pie Chart widget
-            // PieChartWidget(),
+            DailyPieChart(date: _selectedDate),
 
             // TODO: Add Total Calories Display
             // TotalCaloriesDisplay(),
 
             // TODO: Add Table Calendar
-            // TableCalendarWidget(),
+            TableCalendar(
+              firstDay: _firstDay,
+              lastDay: _lastDay,
+              focusedDay: _selectedDate,
+              calendarFormat: CalendarFormat.month,
+              onDaySelected: (selectedDay, focusedDay) {
+                setState(() {
+                  _selectedDate = selectedDay;
+                });
+              },
+            ),
 
             // TODO: Add Meal Information
             // MealInformationWidget(),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Navigate to the AddCaloriesPage when the FAB is pressed
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AddCaloriesPage()),
+          );
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
