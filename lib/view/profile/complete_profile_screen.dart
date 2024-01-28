1576ee9a8c626f3a242044ca5e1ed9dff1c13bce
ping0807 <93776380+ping0807@users.noreply.github.com>
2024-01-28 15:14:37 +0800

    bmi

--- lib/view/profile/complete_profile_screen.dart
+++ lib/view/profile/complete_profile_screen.dart
@@ -269,7 +269,7 @@ String? determineBMIGroup(double bmi) {
     return 'Underweight';
   } else if (bmi >= 18.5 && bmi <= 24.9) {
     return 'Normal';
-  } else if (bmi >= 25 && bmi <= 29.9) {
+  } else if (bmi >= 25) {
     return 'Overweight';
   }
 }
